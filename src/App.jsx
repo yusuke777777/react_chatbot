177b0517d39bd6b98cd78cf2c9c2fc606f16ffed
yusuke777777 <yusuke2370@gmail.com>
2025-05-04 15:44:52 +0900

    更新

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import defaultDataset from './dataset';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      answers: [],
+      chats:[],
+      currentId: "init",
+      dataset:{defaultDataset},
+      open: false
+    }
+  }
+  render() {
+    return (
+    <div>
+      <section className='.c-secction'>
+        <div className='.c-box'>
+        </div>
+      </section>
+    </div>
+    );
+  }
+}
+
